25e9624101e7cb78dbd49893d1cb541f83f374a5
Max Tyler <iam4x@me.com>
2016-06-16 23:49:05 +0200

    style(core/connect): more explicit copy static methods

--- core/connect.js
+++ core/connect.js
@@ -1,13 +1,12 @@
-import { isString, isArray } from 'lodash'
+import { isFunction, isString, isArray } from 'lodash'
 import { observer } from 'mobx-react'
 import { inject } from 'react-tunnel'
 
-const excludedProps = Object.getOwnPropertyNames(() => undefined)
+const nativeObjectMethods = Object.getOwnPropertyNames(() => undefined)
 
 // borrowed from `react-redux/connect`
 // https://github.com/rackt/react-redux/blob/master/src/components/connect.js#L17
-const getDisplayName = (WrappedComponent) =>
-  WrappedComponent.displayName || WrappedComponent.name || 'Component'
+const getDisplayName = ({ displayName, name }) => displayName || name || 'Component'
 
 export default (params) => (Component) => {
   // * connect('foo' || [ 'foo', 'bar' ]) -> provide storeKey data into props
@@ -32,11 +31,13 @@ export default (params) => (Component) => {
   const FinalComponent = injectStoresIntoProps(observer(Component))
 
   // copy missing static methods from `Component` to `FinalComponent`
-  Object.getOwnPropertyNames(Component).forEach((prop) => {
-    if (excludedProps.indexOf(prop) < 0 &&
-        typeof Component[prop] === 'function' &&
-        !FinalComponent[prop]) {
-      FinalComponent[prop] = Component[prop]
+  Object.getOwnPropertyNames(Component).forEach(method => {
+    const isNotNativeMethod = !nativeObjectMethods.includes(method)
+    const isNotAlreadyDefined = !isFunction(FinalComponent[method])
+    const methodIsFunction = isFunction(Component[method])
+
+    if (isNotNativeMethod && methodIsFunction && isNotAlreadyDefined) {
+      FinalComponent[method] = Component[method]
     }
   })
 
