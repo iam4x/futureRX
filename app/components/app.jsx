d8cea43f33bdf0d892f4880834c1050be55131e4
Max Tyler <iam4x@me.com>
2016-06-16 19:38:35 +0200

    chore: add `mobx-react-devtools`

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,3 +1,19 @@
-const App = ({ children }: { children: any }) => (children)
+import React from 'react'
+import DevTools from 'mobx-react-devtools'
+
+type Props = {
+  children: any
+}
+
+const { NODE_ENV } = process.env
+
+const App = ({ children }: Props) => (
+  <div className='app'>
+    { children }
+
+    { NODE_ENV === 'development' &&
+      <DevTools position={ { bottom: 0, right: 20 } } /> }
+  </div>
+)
 
 export default App
