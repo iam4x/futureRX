05ed43b9f503059895f9df490cb0873894c4973f
Max Tyler <iam4x@me.com>
2016-06-16 19:37:48 +0200

    feat(core/connect): enhance connect decorator with `displayName`

--- core/connect.js
+++ core/connect.js
@@ -1,29 +1,47 @@
-import { isArray } from 'lodash'
+import { isString, isArray } from 'lodash'
 import { observer } from 'mobx-react'
 import { inject } from 'react-tunnel'
 
-export default (request) => (Component) => {
+const excludedProps = Object.getOwnPropertyNames(() => undefined)
+
+// borrowed from `react-redux/connect`
+// https://github.com/rackt/react-redux/blob/master/src/components/connect.js#L17
+const getDisplayName = (WrappedComponent) =>
+  WrappedComponent.displayName || WrappedComponent.name || 'Component'
+
+export default (params) => (Component) => {
   // * connect('foo' || [ 'foo', 'bar' ]) -> provide storeKey data into props
   //
   // in 'foo' out ({ foo: store('foo') })
   // in ['foo', 'bar'] out ({ foo: store('foo'), bar: store('bar') })
-  const injectStoresIntoProps = inject(({ store }) =>
-    (isArray(request) ? request : [ request ])
-      .reduce((props, storeKey) =>
-        ({ ...props, [storeKey]: store(storeKey) }),
-        {}
-    )
-  )
+  const injectStoresIntoProps = inject(({ store }) => {
+    if (isString(params)) {
+      return ({ [params]: store(params) })
+    }
+
+    if (isArray(params)) {
+      return params.reduce((result, storeKey) =>
+        ({ ...result, [storeKey]: store(storeKey) }))
+    }
+
+    return ({ store })
+  })
 
   // * observe decorated component by `mobx-react::observer`
   // * connect props from context onto decorated component
-  const finalComponent = injectStoresIntoProps(observer(Component))
+  const FinalComponent = injectStoresIntoProps(observer(Component))
 
-  // if we have `fetchData` static property declared on inital component
-  // copy it to the final component returned
-  if (typeof Component.fetchData === 'function') {
-    Object.assign(finalComponent, { fetchData: Component.fetchData })
-  }
+  // copy missing static methods from `Component` to `FinalComponent`
+  Object.getOwnPropertyNames(Component).forEach((prop) => {
+    if (excludedProps.indexOf(prop) < 0 &&
+        typeof Component[prop] === 'function' &&
+        !FinalComponent[prop]) {
+      FinalComponent[prop] = Component[prop]
+    }
+  })
 
-  return finalComponent
+  // set `connect(Component)` as displayName
+  FinalComponent.displayName = `connect(${getDisplayName(Component)})`
+
+  return FinalComponent
 }
