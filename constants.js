2fe875a50a3bfb6347e3d603ab61c3244bbcaae2
Max Tyler <iam4x@me.com>
2016-06-30 16:57:32 +0200

    refactor(constants): export more constants

--- constants.js
+++ constants.js
@@ -1,2 +1,25 @@
+const { NODE_ENV = 'development' } = process.env
+
+// WEBPACK BUILD CONSTANTS
 export const JS_REGEX = /\.js$|\.jsx$|\.es6$|\.babel$/
 export const EXCLUDE_REGEX = /node_modules/
+
+export const AUTOPREFIXER_BROWSERS = [
+  'Android 2.3',
+  'Android >= 4',
+  'Chrome >= 35',
+  'Firefox >= 31',
+  'Explorer >= 9',
+  'iOS >= 7',
+  'Opera >= 12',
+  'Safari >= 7.1'
+]
+
+export const CLIENT_GLOBALS = {
+  'process.env': {
+    BROWSER: JSON.stringify(true),
+    NODE_ENV: JSON.stringify(NODE_ENV)
+  }
+}
+
+export const isDev = NODE_ENV === 'development'
