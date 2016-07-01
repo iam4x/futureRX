78b982ed3ac179d79eafc8240ce3291527bf7513
Max Tyler <iam4x@me.com>
2016-07-02 00:58:51 +0200

    feat(navbar): add github buttons & link

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,5 +1,10 @@
+/* eslint max-len: 0 */
+
 import React from 'react'
+import withStyles from 'isomorphic-style-loader/lib/withStyles'
 import { Link } from 'react-router'
+
+import styles from 'app/styles/navbar.css'
 
 type Props = {
   children: any
@@ -18,7 +23,18 @@ const App = ({ children }: Props) =>
             <Link to='/'>About</Link>
           </li>
           <li>
-            <Link to='/example'>Todos</Link>
+            <Link to='/example'>Todo App</Link>
+          </li>
+          <li>
+            <a href='https://github.com/iam4x/futureRX'>
+              Github
+            </a>
+          </li>
+        </ul>
+
+        <ul className='nav navbar-nav navbar-right'>
+          <li className={ styles.github }>
+            <span dangerouslySetInnerHTML={ { __html: '<a class="github-button" href="https://github.com/iam4x/futureRX" data-icon="octicon-star" data-style="mega" data-count-href="/iam4x/futureRX/stargazers" data-count-api="/repos/iam4x/futureRX#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star iam4x/futureRX on GitHub">Star</a><a class="github-button" href="https://github.com/iam4x/futureRX/fork" data-icon="octicon-git-branch" data-style="mega" data-count-href="/iam4x/futureRX/network" data-count-api="/repos/iam4x/futureRX#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork iam4x/futureRX on GitHub">Fork</a>' } } />
           </li>
         </ul>
       </div>
@@ -28,4 +44,4 @@ const App = ({ children }: Props) =>
     <div className='container'>{ children }</div>
   </div>
 
-export default App
+export default withStyles(styles)(App)
--- server/server-html.jsx
+++ server/server-html.jsx
@@ -14,7 +14,7 @@ const ServerHTML = ({ assets, locale, body, appState, css }: Props) => (
       <meta charSet='utf-8' />
       <link rel='icon' type='image/ico' href='/favicon.ico' />
 
-      { /* Styles */ }
+      { /* Global bundled styles */ }
       { assets.style.map((href, idx) =>
         <link key={ idx } rel='stylesheet' href={ href } />) }
 
@@ -26,8 +26,16 @@ const ServerHTML = ({ assets, locale, body, appState, css }: Props) => (
     <body>
       <div id='app--container' dangerouslySetInnerHTML={ { __html: body } } />
 
+      { /* bundled js + app state */ }
       <script dangerouslySetInnerHTML={ { __html: `window.__appState__ = ${appState}` } } />
       <script src={ assets.script[0] } />
+
+      { /* external js */ }
+      <script
+        async={ true }
+        defer={ true }
+        id='github-bjs'
+        src='https://buttons.github.io/buttons.js' />
     </body>
   </html>
 )
