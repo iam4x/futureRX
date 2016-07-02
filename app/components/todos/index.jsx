3319d621d3987cb01c8e86923c26e07bbe1e774a
Max Tyler <iam4x@me.com>
2016-07-02 13:43:00 +0200

    feat(todos): add todo count

--- app/components/todos/index.jsx
+++ app/components/todos/index.jsx
@@ -1,4 +1,5 @@
 import { filter } from 'lodash'
+import pluralize from 'pluralize'
 
 import React from 'react'
 import withStyles from 'isomorphic-style-loader/lib/withStyles'
@@ -18,39 +19,65 @@ type Props = {
   }
 }
 
-const Todos = ({ todos }: Props) =>
-  <div className='row'>
-    { /* unfinished todos */ }
-    <div className='col-sm-6'>
-      <div className={ styles.todosContainer }>
-        <h1 className='text-center'>Todos</h1>
+const handleMarkAllDone = (todos) => () =>
+  todos.replace(todos.map(t => ({ ...t, finished: true })))
 
-        <CreateTodo />
+const Todos = ({ todos }: Props) => {
+  const pendingTodos = filter(todos, { finished: false })
+  const finishedTodos = filter(todos, { finished: true })
 
-        <ul className={ styles.todosList }>
-          { filter(todos, { finished: false }).map(todo =>
-            <PendingTodo
-              key={ todo.id }
-              todo={ todo } />) }
-        </ul>
+  return (
+    <div className='row'>
+      { /* unfinished todos */ }
+      <div className='col-sm-6'>
+        <div className={ styles.todosContainer }>
+          <h1 className='text-center'>Todos</h1>
+
+          <CreateTodo />
+
+          <div className='form-group'>
+            <div
+              className='btn btn-block btn-sm btn-success'
+              onClick={ handleMarkAllDone(todos) }>
+              Mark all done
+            </div>
+          </div>
+
+          <ul className={ styles.todosList }>
+            { pendingTodos.map(todo =>
+              <PendingTodo
+                key={ todo.id }
+                todo={ todo } />) }
+          </ul>
+
+          <div className={ styles.count }>
+            { pendingTodos.length ?
+              <span>
+                <strong>{ pendingTodos.length }</strong>
+                <span> { pluralize('todo', pendingTodos.length) } remaining</span>
+              </span> :
+              <span>Congrats it's all done!</span> }
+          </div>
+        </div>
+      </div>
+
+      { /* finished todos */ }
+      <div className='col-sm-6'>
+        <div className={ styles.todosContainer }>
+          <h1 className='text-center'>Already done</h1>
+
+          <ul className={ styles.todosList }>
+            { finishedTodos.map(todo =>
+              <FinishedTodo
+                key={ todo.id }
+                todo={ todo }
+                onRemove={ () => todos.remove(todo) } />) }
+          </ul>
+        </div>
       </div>
     </div>
-
-    { /* finished todos */ }
-    <div className='col-sm-6'>
-      <div className={ styles.todosContainer }>
-        <h1 className='text-center'>Already done</h1>
-
-        <ul className={ styles.todosList }>
-          { filter(todos, { finished: true }).map(todo =>
-            <FinishedTodo
-              key={ todo.id }
-              todo={ todo }
-              onRemove={ () => todos.remove(todo) } />) }
-        </ul>
-      </div>
-    </div>
-  </div>
+  )
+}
 
 
 export default Object.assign(
