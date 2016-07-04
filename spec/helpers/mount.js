0277c478e854eed520649a9ff59c0623715b1671
Max Tyler <iam4x@me.com>
2016-07-05 01:43:21 +0200

    test: add first test on `App` component

---
+++ spec/helpers/mount.js
@@ -0,0 +1,21 @@
+import { noop } from 'lodash'
+
+import React from 'react'
+import { mount } from 'enzyme'
+import { Provider } from 'react-tunnel'
+
+import createStore from 'app/store'
+import ProvideInsertCss from 'core/provide-insert-css'
+
+export default (Component, props = {}) => {
+  const store = createStore()
+  const wrapper = mount(
+    <Provider provide={ { store } }>
+      { () =>
+        <ProvideInsertCss insertCss={ noop }>
+          <Component { ...props } />
+        </ProvideInsertCss> }
+    </Provider>
+  )
+  return { store, wrapper }
+}
