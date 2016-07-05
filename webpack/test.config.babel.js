bcf798bae81dec55eb18b3c8039aa959ee12634a
Max Tyler <iam4x@me.com>
2016-07-05 18:50:42 +0200

    refactor: webpack configs

--- webpack/test.config.babel.js
+++ webpack/test.config.babel.js
@@ -1,7 +1,11 @@
-const { module: { loaders } } = require('./shared.config')
-
-module.exports = {
-  module: { loaders },
+export default {
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.css$/, loader: 'css?modules!postcss' },
+      { test: /\.md$/, loader: 'raw' }
+    ]
+  },
   output: { libraryTarget: 'commonjs2' },
   resolve: {
     extensions: [ '', '.js', '.json', '.jsx' ]
