a0b8251ca64c3858fea5c8f5c9e9cdbf2b49e9e4
Max Tyler <iam4x@me.com>
2016-07-06 23:46:13 +0200

    fix: `withStyles()` calls `setTimeout` into `componentWillMount()`

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -12,12 +12,10 @@ test('it should display basic layout', () => {
   const { wrapper } = mount(App)
   expect(wrapper.find('.navbar-brand')).to.exist
   expect(wrapper.find('.navbar-right')).to.exist
-  wrapper.unmount()
 })
 
 test('it should render child', () => {
   const { wrapper } = mount(App, { children: <h1>Foobar</h1> })
   expect(wrapper.find('h1')).to.exist
   expect(wrapper.find('h1')).to.have.text('Foobar')
-  wrapper.unmount()
 })
