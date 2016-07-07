af1df72574f22a04c526a47aff851855e0c86658
Max Tyler <iam4x@me.com>
2016-07-07 16:36:16 +0200

    test(core/connect): test without params && an array as params

--- spec/core/connect.spec.js
+++ spec/core/connect.spec.js
@@ -12,12 +12,14 @@ import connect from 'core/connect'
 chai.use(chaiEnzyme())
 
 const Example = ({ todos }: { todos: any }) =>
-  <ul>
-    { todos.map(({ title }) =>
-      <li key={ title }>{ title }</li>) }
-  </ul>
+  <ul>{ todos.map(({ title }) => <li key={ title }>{ title }</li>) }</ul>
 
 Example.fetchData = noop
+
+const Example2 = ({ store }: { store: any }) =>
+  <ul>{ store('todos').map(({ title }) => <li key={ title }>{ title }</li>) }</ul>
+
+const data = { todos: [ { title: 'foo' } ] }
 
 test('it should decorate component', t => {
   const Decorated = connect('todos')(Example)
@@ -26,8 +28,24 @@ test('it should decorate component', t => {
 })
 
 test('it should connect store with store key', () => {
-  const store = createStore({ todos: [ { title: 'foo' } ] })
+  const store = createStore(data)
   const Decorated = connect('todos')(Example)
+  const { wrapper } = mount(Decorated, {}, store)
+  expect(wrapper.find('li')).to.exist
+  expect(wrapper.find('li')).to.have.text('foo')
+})
+
+test('it should connect whole store without params', () => {
+  const store = createStore(data)
+  const Decorated = connect()(Example2)
+  const { wrapper } = mount(Decorated, {}, store)
+  expect(wrapper.find('li')).to.exist
+  expect(wrapper.find('li')).to.have.text('foo')
+})
+
+test('it should connect with an array as params', () => {
+  const store = createStore(data)
+  const Decorated = connect([ 'todos' ])(Example)
   const { wrapper } = mount(Decorated, {}, store)
   expect(wrapper.find('li')).to.exist
   expect(wrapper.find('li')).to.have.text('foo')
